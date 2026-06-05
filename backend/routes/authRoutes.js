c1b106347b4fff62185339f0db970a8f0589396e
Aaditya Sharma <sharmaa50045@gmail.com>
2026-06-05 22:44:57 +0530

    changing code

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser, logoutUser } = require('../controllers/authController.js');
+const { protect } = require('../middleware/authMiddleware.js')
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/user", Protect, admin, getUsers);
+router.get("/user", protect, getUsers);
